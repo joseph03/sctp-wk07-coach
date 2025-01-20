f9942c0e1f539acacaca75230c9284dc9094e2bc
jt-ub22-legn7 <i7ub22jt@Legn7>
2025-01-20 20:28:45 +0800

    2nd

--- backend.tf
+++ backend.tf
@@ -1,6 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce9-tfstate" 
+    #at the end of terraform apply or terrafrom destroy
+    #tfstate will be updated.
     key    = "joseph-ce9-module2-lesson2.tfstate" # Replace the value of key to <your suggested name>.tfstate for example terraform-ex-ec2-<NAME>.tfstate
     region = "us-east-1"
   }
